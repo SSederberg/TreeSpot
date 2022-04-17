6ec5be7258600364064dedc72aece40652e643e9
Spencer S <SSederberg@users.noreply.github.com>
2022-04-16 23:39:57 -0500

    Use interface in ActivityUtil.kt instead of object, GetFavoriteUserSpotsQuery.kt is now a abstract query.

--- app/src/main/java/net/n4dev/treespot/db/query/GetFavoriteUserSpotsQuery.kt
+++ app/src/main/java/net/n4dev/treespot/db/query/GetFavoriteUserSpotsQuery.kt
@@ -1,17 +1,22 @@
 package net.n4dev.treespot.db.query
 
-import io.objectbox.query.Query
+import io.objectbox.Property
+import io.objectbox.query.QueryCondition
+import net.n4dev.treespot.core.AbstractQuery
 import net.n4dev.treespot.core.entity.FavoriteSpot
-import net.n4dev.treespot.core.entity.FavoriteSpot_
-import net.n4dev.treespot.db.TreeSpotObjectBox
+import net.n4dev.treespot.db.constants.TreeSpotFavoriteConstants
 
-class GetFavoriteUserSpotsQuery {
+class GetFavoriteUserSpotsQuery(private val userID : String) : AbstractQuery<FavoriteSpot>(FavoriteSpot::class.java) {
 
-    companion object {
-        private val queyrBox = TreeSpotObjectBox.getBox(FavoriteSpot::class.java)
-
-        fun get(userID : String) : Query<FavoriteSpot> {
-            return queyrBox.query(FavoriteSpot_.favoriteUserID.equal(userID)).build()
-        }
+    //    companion object {
+//        private val queyrBox = TreeSpotObjectBox.getBox(FavoriteSpot::class.java)
+//
+//        fun get(userID : String) : Query<FavoriteSpot> {
+//            return queyrBox.query(FavoriteSpot_.favoriteUserID.equal(userID)).build()
+//        }
+//    }
+    override fun buildConditions(fields: Array<out Property<FavoriteSpot>>): QueryCondition<FavoriteSpot> {
+        val field = FavoriteSpot.fieldConverter.get(TreeSpotFavoriteConstants.SPOT_FAV_USER_ID) as Int
+        return fields[field].equal(userID)
     }
 }
